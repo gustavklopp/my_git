686986937109c4662496592b4a47c509dd273d74
gustavklopp <mars@mykolab.com>
2015-03-22 16:34:29 +0100

    fin de week end

--- workspace/Dicomedoc/Dicomedoc/dico_app/views/dci_page.py
+++ workspace/Dicomedoc/Dicomedoc/dico_app/views/dci_page.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # view for index page.
 def page(request):
-    return render(request, 'index.html')
+    if 'search_text' in request.GET:
+        name = request.GET.get('search_text', '')
+    return render(request, 'dci_page.html', {'name':name})
 
