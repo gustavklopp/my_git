66c1c82354305cbb12800e108f268f133d27cf87
gustavklopp <mars@mykolab.com>
2015-03-22 18:16:00 +0100

    end of week end 2

--- workspace/Dicomedoc/Dicomedoc/dico_app/views/dci_page.py
+++ workspace/Dicomedoc/Dicomedoc/dico_app/views/dci_page.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from dico_app.models import RcpTable
 
 # view for index page.
 def page(request):
     if 'search_text' in request.GET:
         name = request.GET.get('search_text', '')
+    drug_found = RcpTable.objects.get()
     return render(request, 'dci_page.html', {'name':name})
 
